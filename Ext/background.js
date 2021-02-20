757ce92b97c508238fb41fa2c4fd6d54513cbb00
Aneel Singh <ajsingh1012@gmail.com>
2021-02-20 11:52:59 -0700

    Add initial extension

---
+++ Ext/background.js
@@ -0,0 +1,9 @@
+/*
+let color = '#311757';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({color});
+  console.log('hello');
+  console.log(color);
+});
+*/
---
+++ Ext/modules.js
@@ -0,0 +1,4 @@
+//import Recorder from '../opus-recorder-master';
+//import encoderPath from '../opus-recorder-master/dist/encoderWorker.min.js';
+
+//const rec = new Recorder({encoderPath});
