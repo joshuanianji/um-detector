fb81c34b8e645dee3df161f413f97df322a6cc82
RubberNoodles <ocheng@ualberta.ca>
2021-02-20 13:56:11 -0700

    :tada: testing audio pipe to firebase

--- firebase/testing/functions/index.js
+++ firebase/testing/functions/index.js
@@ -1,14 +1,30 @@
 const functions = require("firebase-functions");
 
+const express = require('express');
+const app = express();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
+
 //
+
+app.post('/audio', () => {
+    return;
+});
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
    functions.logger.info("Hello logs!", {structuredData: true});
    response.send("Hello from Firebase!");
  });
 
+
  exports.onStorageUpdate = functions.storage.object().onFinalize( async (object) => {
     functions.logger.info("Hi trigger logs!", {structuredData: true});
-    response.send("hello");
+    console.log("wow the trigger actually fired");
+
+    const fileBucket = object.bucket; // The Storage bucket that contains the file.
+    const filePath = object.name; // File path in the bucket.
+    const contentType = object.contentType; // File content type.
+    const metageneration = object.metageneration; // Number of times metadata has been generated. New objects have a value of 1.
+
+    console.table({fileBucket, filePath, contentType, metageneration, object});
  });
